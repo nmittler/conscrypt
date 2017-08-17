92225fe8328099c1a1e5e8a7c96511fb9a1ee30d
Nathan Mittler <nmittler@gmail.com>
2017-08-17 15:20:18 -0700

    Switch default to engine-based socket
    Fixes #65

--- common/src/main/java/org/conscrypt/SSLUtils.java
+++ common/src/main/java/org/conscrypt/SSLUtils.java
@@ -59,7 +59,7 @@ import javax.security.cert.CertificateException;
  */
 final class SSLUtils {
     static final boolean USE_ENGINE_SOCKET_BY_DEFAULT = Boolean.parseBoolean(
-            System.getProperty("org.conscrypt.useEngineSocketByDefault", "false"));
+            System.getProperty("org.conscrypt.useEngineSocketByDefault", "true"));
     private static final int MAX_PROTOCOL_LENGTH = 255;
 
     // TODO(nathanmittler): Should these be in NativeConstants?
